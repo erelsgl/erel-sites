a5e277da1eb5b8b977e745ca878eba8dbdf9a709
Erel Segal-Halevi <erelsgl@gmail.com>
2014-09-12 07:08:55 +0000

    manua,

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -1528,3 +1528,26 @@ SET character_set_connection=hebrew;
 			)
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/klli/mdrjim/rmbm0.html',
+				'tnk1/klli/mdrjim/rmbm7.html',
+				NULL,
+				'2014-09-12 07:02:03',
+				'2014-09-12 07:02:03',
+				'שבע מצוות בני נוח הן תנאי לשלום',
+				'אראל',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2014-09-12 07:02:03'
+			WHERE ktovt_bn='tnk1/klli/mdrjim/rmbm0.html';
+
