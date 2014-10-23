5a650e96fe1bdbf56ac15dd2329e49134f98c756
erelsgl <erelsgl@erelsgl-H61MLC.(none)>
2014-10-23 23:02:23 +0300

    from home

--- wiki/mikra_lfi_hamsora/2_mysql2tsv.php
+++ wiki/mikra_lfi_hamsora/2_mysql2tsv.php
@@ -7,7 +7,7 @@ $rows = sql_query_or_die("
 	SELECT *
 		FROM psuqim_dovi
 		WHERE 0
-			OR chapter_id LIKE 'מגילת%'
+			OR chapter_id LIKE 'ספר דברי %'
 		ORDER BY book_code_mamre, chapter_number, verse_number
 		");
 
