cdce888bacf0b223e58a4e09b6457298d9cd7a2a
Erel Segal-Halevi <erelsgl@gmail.com>
2014-10-25 08:00:01 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -2110,3 +2110,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0102_9.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_627.html',
+				NULL,
+				'2014-10-24 12:31:09',
+				'2014-10-24 12:31:09',
+				'נח נוטע כרם/ שיר מאת: אהובה קליין(c)',
+				'אהובה קליין.',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2014-10-24 12:31:09'
+			WHERE ktovt_bn='tnk1/forums.html';
+
