699e60d0a0548a9a4716d1e0a6c70b14f610d5f3
Erel Segal-Halevi <erelsgl@gmail.com>
2014-11-01 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -2388,3 +2388,19 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/msr/knesset/land.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/prqim/t0112.htm',
+				'tnk1/messages/prqim_t0112_8.html',
+				NULL,
+				'2014-10-31 11:06:06',
+				'2014-10-31 11:06:06',
+				'\"לך לך\" לפי הפרד\"ס',
+				'חגי הופר',
+				''
+			)
+		;
+
