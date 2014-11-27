8368e1eba57ad678afe46aa56a9704e57c5fc4d7
erelsgl <erelsgl@gmail.com>
2014-11-27 10:34:24 +0200

    add column f

--- wiki/mikra_lfi_hamsora/2_mysql2tsv.php
+++ wiki/mikra_lfi_hamsora/2_mysql2tsv.php
@@ -10,6 +10,11 @@ $rows = sql_query_or_die("
 			OR chapter_id LIKE 'ספר דניאל%'
 			OR chapter_id LIKE 'ספר עזרא%'
 			OR chapter_id LIKE 'ספר דברי %'
+			
+--			OR chapter_id LIKE 'ספר יהושע%'
+--			OR chapter_id LIKE 'ספר שופטים%'
+--			OR chapter_id LIKE 'ספר שמואל %'
+--			OR chapter_id LIKE 'ספר מלכים %'
 		ORDER BY book_code_mamre, chapter_number, verse_number
 		");
 
@@ -18,7 +23,7 @@ $file = fopen($FILENAME_OUTPUT,"w");
 //fwrite($file, "<poem>\n<nowiki>\n");
 //fwrite($file, "קוד-ספר${SEP}אות-פרק${SEP}אות-פסוק${SEP}קוד-לפני-פסוק${SEP}טקסט-פסוק"."\r\n");
 while ($row = sql_fetch_assoc($rows)) {
-	fwrite($file, "$row[chapter_id]$SEP$row[verse_letter]$SEP$row[prefix]$SEP$row[verse_letter_text]$SEP$row[verse_text]\r\n");
+	fwrite($file, "$row[chapter_id]$SEP$row[verse_letter]$SEP$row[prefix]$SEP$row[verse_letter_text]$SEP$row[verse_text]$SEP$row[stylized_text]\r\n");
 }
 //fwrite($file, "</nowiki>\n</poem>\n");
 fclose($file);
