bb404565ea86cfcbe6b9886a3852e10693270c41
Erel Segal-Halevi <erelsgl@gmail.com>
2015-01-23 08:00:02 +0000

    daily backup

--- tnk1/_magr/gfc_users.sql
+++ tnk1/_magr/gfc_users.sql
@@ -2709,3 +2709,9 @@ SET character_set_connection=utf8;
 		REPLACE  INTO gfc_users(created_at,id,name)
 		VALUES('2015-01-22 06:08:38','https://www.google.com/accounts/o8/id?id=AItOawkimwFyqevXh2gjrVPHX9qj0eFdlDWJLww','Daian Moshe');
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		REPLACE  INTO gfc_users(created_at,id,name)
+		VALUES('2015-01-22 16:05:50','https://www.google.com/accounts/o8/id?id=AItOawkimwFyqevXh2gjrVPHX9qj0eFdlDWJLww','Daian Moshe');
+
