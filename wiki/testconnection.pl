bada07b00624fc930e671eb3a2b508994530a71e
erelsgl <erelsgl@erelsgl-H61MLC.(none)>
2015-02-09 23:00:02 +0200

    from home

---
+++ wiki/testconnection.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+# Test the Internet connection
+
+$URL = "http://www.google.com";
+
+use LWP::Simple;
+my $browser = LWP::UserAgent->new();
+
+my @ns_headers = (
+	'User-Agent' => "Perl browser",
+	'Accept' => 'image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, image/png, */*',
+	'Accept-Charset' => 'iso-8859-1,*,utf-8',
+	'Accept-Language' => 'en-US',
+);
+
+for ($i=0; $i<10; ++$i) {
+	my $response=$browser->get($URL);
+	if ($response->content =~ /Google/) {
+		print "Google loaded\n";
+	} else {
+		print $response->content;
+	}
+}
