f98c0d0dca9eef5f3611ebedfd4157fef57a75af
erelsgl <erelsgl@erelsgl-H61MLC.(none)>
2015-04-07 23:00:03 +0300

    from home

--- tnk1/_magr/sugy_sugim_tnk1.sql
+++ tnk1/_magr/sugy_sugim_tnk1.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `sugy_sugim_tnk1` (
-  `sug_prti` varchar(16) NOT NULL default '',
-  `sug_klli` varchar(16) NOT NULL default '',
-  PRIMARY KEY  (`sug_prti`)
+  `sug_prti` varchar(16) NOT NULL DEFAULT '',
+  `sug_klli` varchar(16) NOT NULL DEFAULT '',
+  PRIMARY KEY (`sug_prti`)
 ) ENGINE=MyISAM CHARACTER SET utf8;
 
 SET character_set_database=utf8;
