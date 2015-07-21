9622823daad303499dc1b5dfcb511ac21a722e48
Erel Segal-Halevi <erelsgl@gmail.com>
2015-07-21 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -4304,3 +4304,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/tora/jmot/jm-05-17.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sig/9jir.html',
+				'tnk1/messages/sig_9jir_16.html',
+				NULL,
+				'2015-07-20 10:55:56',
+				'2015-07-20 10:55:56',
+				'תן לי את יבנה וחכמיה/ שיר לתשעה באב',
+				'חגי הופר',
+				'hagaihof @ gmail.com'
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2015-07-20 10:55:56'
+			WHERE ktovt_bn='tnk1/sig/9jir.html';
+
