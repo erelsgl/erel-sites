dc54ae2e854f75bef451bb1b4f2d4c99b4205629
Erel Segal-Halevi <erelsgl@gmail.com>
2015-07-31 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -4673,3 +4673,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/sofrim_hoffer_index_2.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/index.html',
+				'tnk1/messages/sofrim_index_49.html',
+				NULL,
+				'2015-07-30 15:17:17',
+				'2015-07-30 15:17:17',
+				'באש או ביש?',
+				'אבנר רמו',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2015-07-30 15:17:17'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
