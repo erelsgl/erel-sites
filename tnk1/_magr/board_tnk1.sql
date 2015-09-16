93f1622b38ca55eeb6de94464d5bbb81369df2c7
Erel Segal-Halevi <erelsgl@gmail.com>
2015-09-16 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -7180,3 +7180,19 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2015-09-13 21:10:52'
 			WHERE ktovt_bn='tnk1/sofrim/index.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sig/9sfr.html',
+				'tnk1/messages/sig_9sfr_76.html',
+				NULL,
+				'2015-09-15 17:17:03',
+				'2015-09-15 17:17:03',
+				'על \"באמת ובאהבה\" מאת אבי רט',
+				'חגי הופר',
+				''
+			)
+		;
+
