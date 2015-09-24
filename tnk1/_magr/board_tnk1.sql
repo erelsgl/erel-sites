171647e71ab84245d7efe47071edfa498a780769
Erel Segal-Halevi <erelsgl@gmail.com>
2015-09-24 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -7501,3 +7501,49 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2015-09-22 16:42:16'
 			WHERE ktovt_bn='tnk1/sofrim/index.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_726.html',
+				NULL,
+				'2015-09-23 19:56:57',
+				'2015-09-23 19:56:57',
+				' נשר כמשל/ שיר מאת: אהובה קליין(c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2015-09-23 19:56:57'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_727.html',
+				NULL,
+				'2015-09-23 20:03:37',
+				'2015-09-23 20:03:37',
+				'פרשת האזינו- מה מסמל הנשר ובמה ייחודו?/מאת: אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2015-09-23 20:03:37'
+			WHERE ktovt_bn='tnk1/forums.html';
+
