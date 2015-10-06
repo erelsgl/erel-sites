e0fe1e7baa7d8560af230ce84d273102be4c85b8
Erel Segal-Halevi <erelsgl@gmail.com>
2015-10-06 08:00:01 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -7895,3 +7895,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/2593.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_730.html',
+				NULL,
+				'2015-10-05 21:12:09',
+				'2015-10-05 21:12:09',
+				'פרשת בראשית- מדוע גורשו אדם וחוה מגן עדן?מאת: אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2015-10-05 21:12:09'
+			WHERE ktovt_bn='tnk1/forums.html';
+
