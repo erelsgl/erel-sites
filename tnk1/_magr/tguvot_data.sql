ad6a42ec0352f6b21e105b7481fa20471926badd
Erel Segal-Halevi <erelsgl@gmail.com>
2016-05-25 08:00:04 +0000

    daily backup

--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -995,3 +995,41 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/kma/qjrim1/1xy.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t3007_1.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2016-05-24 14:31:05'
+		WHERE parent='tnk1/messages/prqim_t3007_1.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+			UPDATE tguvot_data
+			SET count=count-1, updated_at='2016-05-24 14:31:09'
+			WHERE parent='tnk1/messages/prqim_t3007_1.html'
+			;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t3007_1.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2016-05-24 14:31:37'
+		WHERE parent='tnk1/messages/prqim_t3007_1.html'
+		;
+
