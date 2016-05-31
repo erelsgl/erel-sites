076287d7297d97fe261c8fe3acb2a3a0b185eb07
Erel Segal-Halevi <erelsgl@gmail.com>
2016-05-31 08:00:03 +0000

    daily backup

--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -1093,3 +1093,41 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/messages/prqim_t0707_0.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/tora/wyqra/wy-27-2829.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2016-05-30 11:26:38'
+		WHERE parent='tnk1/tora/wyqra/wy-27-2829.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+			UPDATE tguvot_data
+			SET count=count-1, updated_at='2016-05-30 11:37:16'
+			WHERE parent='tnk1/tora/wyqra/wy-27-2829.html'
+			;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/tora/wyqra/wy-27-2829.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2016-05-30 11:42:02'
+		WHERE parent='tnk1/tora/wyqra/wy-27-2829.html'
+		;
+
