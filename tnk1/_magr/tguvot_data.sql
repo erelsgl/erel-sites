cd9afb4589375304d0ee1237a5261ae406bb1fc2
Erel Segal-Halevi Home <erelsgl@gmail.com>
2016-11-21 08:00:03 +0000

    daily backup

--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -2452,3 +2452,48 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/klli/mdrjim/wyhi.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t0115_3.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2016-11-20 16:35:41'
+		WHERE parent='tnk1/messages/prqim_t0115_3.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/msr/knesset/land.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2016-11-21 04:58:43'
+		WHERE parent='tnk1/msr/knesset/land.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t2690_3.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2016-11-21 05:30:34'
+		WHERE parent='tnk1/messages/prqim_t2690_3.html'
+		;
+
