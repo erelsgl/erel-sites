fbf9dd71883bcbdfef7f77a5d4f340a993371bb3
Erel Segal-Halevi Home <erelsgl@gmail.com>
2017-01-06 08:00:04 +0000

    daily backup

--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -3593,3 +3593,18 @@ SET character_set_connection=utf8;
 			WHERE parent='tnk1/klli/mdrjim/ps03.html'
 			;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t09b03_1.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2017-01-06 07:37:39'
+		WHERE parent='tnk1/messages/prqim_t09b03_1.html'
+		;
+
