f90830757afaf99d0e6a2e31ef9171c05bacb889
Erel Segal-Halevi Home <erelsgl@gmail.com>
2017-02-23 08:00:04 +0000

    daily backup

--- tnk1/_magr/tguvot.sql
+++ tnk1/_magr/tguvot.sql
@@ -21876,3 +21876,15 @@ SET character_set_connection=utf8;
 		)
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT INTO tguvot (parent,body,userid,username,created_at) VALUES (
+		'tnk1/tora/brejit/aqda2.html',
+		'<p>תודה רבה על הפרשנות המעניינת :)</p>',
+		'113650349735496733803',
+		'גל סוקוליק',
+		'2017-02-22 16:58:51'
+		)
+		;
+
