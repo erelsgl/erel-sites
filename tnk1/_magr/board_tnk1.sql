e5544c9eb1adb51d17c0b203653fc23096a6c46c
Erel Segal-Halevi Home <erelsgl@gmail.com>
2017-07-26 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -81,3 +81,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/nvia/yjayhu/yj-58-14.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_920.html',
+				NULL,
+				'2017-07-25 20:13:18',
+				'2017-07-25 20:13:18',
+				'פרשת דברים: מאין,כי לא המדרש העיקר,אלא המעשה?/אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2017-07-25 20:13:18'
+			WHERE ktovt_bn='tnk1/forums.html';
+
