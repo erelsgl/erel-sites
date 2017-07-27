5a47b399e93d13ed003942952e9fb96539d3ca77
Erel Segal-Halevi Home <erelsgl@gmail.com>
2017-07-27 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -104,3 +104,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2017-07-25 20:13:18'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_921.html',
+				NULL,
+				'2017-07-26 20:21:30',
+				'2017-07-26 20:21:30',
+				'הגיע העת/שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2017-07-26 20:21:30'
+			WHERE ktovt_bn='tnk1/forums.html';
+
