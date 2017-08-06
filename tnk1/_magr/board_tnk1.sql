28b40675398df2af62c2f54c6bb182f99cbfc34d
Erel Segal-Halevi Home <erelsgl@gmail.com>
2017-08-06 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -318,3 +318,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/tora/wyqra/wy-18-25.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_923.html',
+				NULL,
+				'2017-08-06 05:47:12',
+				'2017-08-06 05:47:12',
+				'תחנוני משה/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2017-08-06 05:47:12'
+			WHERE ktovt_bn='tnk1/forums.html';
+
