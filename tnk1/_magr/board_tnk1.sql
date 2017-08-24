4259eaa45a748cf74bc01e5f880e420c8aa5dac3
Erel Segal-Halevi Home <erelsgl@gmail.com>
2017-08-24 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -4180,3 +4180,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2017-08-22 19:32:07'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_929.html',
+				NULL,
+				'2017-08-23 19:49:15',
+				'2017-08-23 19:49:15',
+				'שופט בדורו/ שיר מאת: אהובה קליין(c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2017-08-23 19:49:15'
+			WHERE ktovt_bn='tnk1/forums.html';
+
