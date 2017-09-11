b195a2cf6468bc87996e6cd4381c3a219b43727f
Erel Segal-Halevi Home <erelsgl@gmail.com>
2017-09-11 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -4598,3 +4598,11 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/sofrim/zuckm/42.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2017-09-11 04:47:19'
+		WHERE ktovt_bn='tnk1/messages/prqim_t25b06_0.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -288,3 +288,18 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/sofrim/zuckm/42.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t25b06_0.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2017-09-11 04:47:19'
+		WHERE parent='tnk1/messages/prqim_t25b06_0.html'
+		;
+
