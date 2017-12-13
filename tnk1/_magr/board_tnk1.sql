62dc56b691e9b8eb41a73bc69929b75698adc248
Erel Segal-Halevi Home <erelsgl@gmail.com>
2017-12-13 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -8640,3 +8640,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0147_3.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_962.html',
+				NULL,
+				'2017-12-12 09:51:51',
+				'2017-12-12 09:51:51',
+				'יוסף מתעלה/ שיר מאת: אהובה קליין(c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2017-12-12 09:51:51'
+			WHERE ktovt_bn='tnk1/forums.html';
+
