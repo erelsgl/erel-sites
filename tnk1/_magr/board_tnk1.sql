ced8d1d6c9744752218564cdb846512c57a8b688
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-01-02 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -9187,3 +9187,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/tora/brejit/sdom.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_966.html',
+				NULL,
+				'2018-01-01 16:27:15',
+				'2018-01-01 16:27:15',
+				' משה בבית פרעה/ שיר מאת: אהובה קליין(c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-01-01 16:27:15'
+			WHERE ktovt_bn='tnk1/forums.html';
+
