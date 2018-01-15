e3726688468ad7c048b8630a94767d700668380b
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-01-15 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -9934,3 +9934,42 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0507_0.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/new.html',
+				'tnk1/messages/new_29.html',
+				NULL,
+				'2018-01-14 16:27:19',
+				'2018-01-14 16:27:19',
+				'עיקרי אמונה או עיקרי ערכים?',
+				'חגי הופר',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_970.html',
+				NULL,
+				'2018-01-14 21:47:47',
+				'2018-01-14 21:47:47',
+				'פרשת בא- במה נחלקו פרעה ועבדיו?/אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-01-14 21:47:47'
+			WHERE ktovt_bn='tnk1/forums.html';
+
