8c7562bf97fa916424d0a1484d8d5c0ae05fec41
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-03-24 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -2524,3 +2524,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0518_2.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_990.html',
+				NULL,
+				'2018-03-23 12:20:20',
+				'2018-03-23 12:20:20',
+				' הכוהן בעבודתו/ שיר מאת: אהובה קליין(c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-03-23 12:20:20'
+			WHERE ktovt_bn='tnk1/forums.html';
+
