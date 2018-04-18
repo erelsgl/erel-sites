3a66d81e365769ab395671f46780bc4ebe840429
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-04-18 08:00:02 +0000

    daily backup

--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -365,3 +365,18 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/kma/qjrim1/ayn.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/dmut_dmut_1381_0.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2018-04-18 07:33:52'
+		WHERE parent='tnk1/messages/dmut_dmut_1381_0.html'
+		;
+
