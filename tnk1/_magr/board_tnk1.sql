e3dd8ef0685148d512cadcdbfb24ee2bdb88a4ee
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-05-24 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -5903,3 +5903,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2018-05-22 20:29:28'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/index.html',
+				'tnk1/messages/agriculture.pdf',
+				NULL,
+				'2018-05-23 22:46:48',
+				'2018-05-23 22:46:48',
+				'Agriculture',
+				'אבנר רמו',
+				'חל יזרעאל - חלקת שדה נבות היזרעאלי'
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-05-23 22:46:48'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
