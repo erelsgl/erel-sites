82497dfba6a1d2387fbb94e64b6ecaf3f67d0006
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-05-29 08:00:01 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -6062,3 +6062,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tryg/messages/mamr_tfilot-eitan_6.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1011.html',
+				NULL,
+				'2018-05-29 05:33:24',
+				'2018-05-29 05:33:24',
+				'פרשת בהעלותך-מה היה שורש חטאם של המתאוננים?/אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-05-29 05:33:24'
+			WHERE ktovt_bn='tnk1/forums.html';
+
