3a468f21df6c8c9d11ee8c0dc77de2163a39ddd7
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-06-17 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -6608,3 +6608,19 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0210_2.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2018-06-16 15:47:27'
+		WHERE ktovt_bn='tryg/messages/sug_mjkb_zkr_0.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2018-06-16 18:31:11'
+		WHERE ktovt_bn='tryg/messages/sug_mjkb_zkr_0.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -2508,3 +2508,33 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/messages/prqim_t0210_2.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tryg/messages/sug_mjkb_zkr_0.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2018-06-16 15:47:27'
+		WHERE parent='tryg/messages/sug_mjkb_zkr_0.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tryg/messages/sug_mjkb_zkr_0.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2018-06-16 18:31:11'
+		WHERE parent='tryg/messages/sug_mjkb_zkr_0.html'
+		;
+
