1127e11e47f1b8e43d992fd3d549b56c33659f16
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-06-20 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -7440,3 +7440,34 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/tora/brejit/ndr.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2018-06-19 14:56:47'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0102_11.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1017.html',
+				NULL,
+				'2018-06-19 20:56:13',
+				'2018-06-19 20:56:13',
+				' פרשת חוקת- מדוע נענשו ישראל במכת הנחשים?/אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-06-19 20:56:13'
+			WHERE ktovt_bn='tnk1/forums.html';
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -2644,3 +2644,18 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/tora/brejit/ndr.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t0102_11.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2018-06-19 14:56:47'
+		WHERE parent='tnk1/messages/prqim_t0102_11.html'
+		;
+
