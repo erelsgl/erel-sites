4e9a8ab52f29d2629b0bbfccdd7791aacd146914
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-07-25 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -9140,3 +9140,57 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/nvia/yxzqel/yx-09-06.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2018-07-24 10:35:31'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0234_1.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1027.html',
+				NULL,
+				'2018-07-24 18:20:43',
+				'2018-07-24 18:20:43',
+				'פרשת ואתחנן- חזון אחרית הימים- כיצד?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-07-24 18:20:43'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/index.html',
+				'tnk1/messages/hard_at_heart.docx',
+				NULL,
+				'2018-07-24 18:51:28',
+				'2018-07-24 18:51:28',
+				'Hard at heart',
+				'אבנר רמו',
+				'avnerramu @ aol.com'
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-07-24 18:51:28'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -4705,3 +4705,18 @@ SET character_set_connection=utf8;
 			WHERE parent='tnk1/nvia/yxzqel/yx-09-06.html'
 			;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t0234_1.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2018-07-24 10:35:31'
+		WHERE parent='tnk1/messages/prqim_t0234_1.html'
+		;
+
