b0691258ed838bbe85e64f559c5a7dcdf990e401
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-10-02 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -13010,3 +13010,34 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/ljon/dq/armit.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2018-10-01 10:23:21'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0146_2.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1044.html',
+				NULL,
+				'2018-10-02 05:08:36',
+				'2018-10-02 05:08:36',
+				'פרשת בראשית- מהו שורש  חטאו של קין?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-10-02 05:08:36'
+			WHERE ktovt_bn='tnk1/forums.html';
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -3660,3 +3660,18 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/ljon/dq/armit.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t0146_2.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2018-10-01 10:23:21'
+		WHERE parent='tnk1/messages/prqim_t0146_2.html'
+		;
+
