89259da8230906faf0866b790190fb28df6d0474
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-10-25 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -13496,3 +13496,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2018-10-23 20:49:19'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1051.html',
+				NULL,
+				'2018-10-24 09:15:22',
+				'2018-10-24 09:15:22',
+				' שרה  וצחוקה/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-10-24 09:15:22'
+			WHERE ktovt_bn='tnk1/forums.html';
+
