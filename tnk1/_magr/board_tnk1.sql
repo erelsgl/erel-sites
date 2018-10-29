0251933886411ce68df13001e6fdc1260a534e4b
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-10-29 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -13527,3 +13527,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/tora/brejit/drk1_sdom.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/prqim/t29.htm',
+				'tnk1/messages/prqim_t29_4.html',
+				NULL,
+				'2018-10-28 15:14:53',
+				'2018-10-28 15:14:53',
+				'מאמר חדש על: רות',
+				'אברהם אלישע',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-10-28 15:14:53'
+			WHERE ktovt_bn='tnk1/prqim/t29.htm';
+
