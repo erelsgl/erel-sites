43ec27b7ffc7de1bdb8c6e5964922e7528ea5481
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-10-30 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -13550,3 +13550,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2018-10-28 15:14:53'
 			WHERE ktovt_bn='tnk1/prqim/t29.htm';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1052.html',
+				NULL,
+				'2018-10-29 20:14:35',
+				'2018-10-29 20:14:35',
+				'פרשת חיי שרה- מי היה אליעזר  ומה הייתה שליחותו?/  אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-10-29 20:14:35'
+			WHERE ktovt_bn='tnk1/forums.html';
+
