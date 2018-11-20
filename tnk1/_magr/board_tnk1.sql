73d018d0923f98725e70ce50777ef9a42ff35c5f
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-11-20 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -14145,3 +14145,49 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/new_40.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1059.html',
+				NULL,
+				'2018-11-20 06:29:42',
+				'2018-11-20 06:29:42',
+				'פרשת וישלח-\"איזהו חכם הרואה את הנולד\"?/אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-11-20 06:29:42'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1060.html',
+				NULL,
+				'2018-11-20 06:34:50',
+				'2018-11-20 06:34:50',
+				' יעקב רוכש חלקת שדה/ שיר מאת: אהובה קליין(c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-11-20 06:34:50'
+			WHERE ktovt_bn='tnk1/forums.html';
+
