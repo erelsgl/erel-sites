1c0fa84496348a4a04533e45f6c1403e87160e5e
Erel Segal-Halevi Home <erelsgl@gmail.com>
2018-12-27 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -15459,3 +15459,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2018-12-26 06:49:41'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1069.html',
+				NULL,
+				'2018-12-26 21:59:08',
+				'2018-12-26 21:59:08',
+				' משה שב למצרים/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2018-12-26 21:59:08'
+			WHERE ktovt_bn='tnk1/forums.html';
+
