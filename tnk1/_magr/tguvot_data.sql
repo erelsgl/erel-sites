ad02ec2b8d25825372b93f4f3746e44ca5d90d1c
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-01-02 08:00:02 +0000

    daily backup

--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -968,3 +968,33 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/messages/prqim_t0617_0.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/ktuv/mj/03-01.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2019-01-01 17:33:02'
+		WHERE parent='tnk1/ktuv/mj/03-01.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/tora_brejit_br-11-02a_1.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2019-01-01 18:37:05'
+		WHERE parent='tnk1/messages/tora_brejit_br-11-02a_1.html'
+		;
+
