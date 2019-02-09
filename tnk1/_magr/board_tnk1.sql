37b5798cfa3e691c179f03fd3383919f07025aa2
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-02-09 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -16608,3 +16608,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t1502_1.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1082.html',
+				NULL,
+				'2019-02-08 12:28:56',
+				'2019-02-08 12:28:56',
+				' תרומת האבנים למשכן/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-02-08 12:28:56'
+			WHERE ktovt_bn='tnk1/forums.html';
+
