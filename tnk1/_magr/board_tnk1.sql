6a781d11473e44ed3d367f82a98310d19a7e7c9f
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-02-13 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -16663,3 +16663,65 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t2690_3.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2019-02-12 16:37:10'
+		WHERE ktovt_bn='tnk1/kma/qjrim1/yra.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2019-02-12 20:21:49'
+		WHERE ktovt_bn='tnk1/kma/qjrim1/yaj.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1083.html',
+				NULL,
+				'2019-02-13 06:33:55',
+				'2019-02-13 06:33:55',
+				'הכהן וקודש הקודשים/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-02-13 06:33:55'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1084.html',
+				NULL,
+				'2019-02-13 07:06:41',
+				'2019-02-13 07:06:41',
+				'פרשת תצווה- הצפון באפוד- ובמעיל הכהן הגדול?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-02-13 07:06:41'
+			WHERE ktovt_bn='tnk1/forums.html';
+
