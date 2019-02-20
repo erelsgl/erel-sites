a593c41776f6ee18dab20b323e7c52af93086a12
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-02-20 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -16883,3 +16883,33 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2019-02-19 06:52:04'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/shabot/index.html',
+				'tnk1/messages/sofrim_shabot_index_122.html',
+				NULL,
+				'2019-02-19 21:01:00',
+				'2019-02-19 21:01:00',
+				'ולתפארת דת משה וישראל',
+				'אלברט שבות',
+				'ashabot @ walla.co.il'
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-02-19 21:01:00'
+			WHERE ktovt_bn='tnk1/sofrim/shabot/index.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-02-19 21:01:00'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
