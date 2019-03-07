0d19b2137dc1897743b204b0d32a54aed54f4961
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-03-07 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -17347,3 +17347,49 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/nvir_dwd_wbt7_0.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1089.html',
+				NULL,
+				'2019-03-06 10:49:28',
+				'2019-03-06 10:49:28',
+				'הענן מעל המשכן/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-03-06 10:49:28'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1090.html',
+				NULL,
+				'2019-03-06 16:36:07',
+				'2019-03-06 16:36:07',
+				' פרשת פקודי- הענן על המשכן- מה תפקידו?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-03-06 16:36:07'
+			WHERE ktovt_bn='tnk1/forums.html';
+
