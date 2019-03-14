e8f17c44bc1f6174897a3d67f6a7f0a6d509abc2
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-03-14 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -17568,3 +17568,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0526_3.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1091.html',
+				NULL,
+				'2019-03-13 14:31:32',
+				'2019-03-13 14:31:32',
+				'פרשת ויקרא- מהו קורבן מנחה- ובמה ייחודו?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-03-13 14:31:32'
+			WHERE ktovt_bn='tnk1/forums.html';
+
