bf42db62dadb0b4476e462a0828f36eaf4998d14
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-06-15 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -21201,3 +21201,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2019-06-13 20:54:58'
 			WHERE ktovt_bn='tnk1/sofrim/index.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1120.html',
+				NULL,
+				'2019-06-14 10:50:50',
+				'2019-06-14 10:50:50',
+				'מינוי הלוויים/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-06-14 10:50:50'
+			WHERE ktovt_bn='tnk1/forums.html';
+
