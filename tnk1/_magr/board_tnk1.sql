4ff76365a97a7f7d71582e0801f002c1afc0171b
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-07-08 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -22029,3 +22029,11 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0109_2.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2019-07-07 11:35:25'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0109_2.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -2244,3 +2244,18 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/messages/prqim_t0109_2.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t0109_2.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2019-07-07 11:35:25'
+		WHERE parent='tnk1/messages/prqim_t0109_2.html'
+		;
+
