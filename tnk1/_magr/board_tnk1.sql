82b6bdb4600690e4aa4ef31d01f49dc4247c4926
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-08-29 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -27132,3 +27132,59 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2019-08-28 04:57:40'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2019-08-28 11:36:45'
+		WHERE ktovt_bn='tnk1/messages/ljon_jorj_zyz_0.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2019-08-28 13:22:35'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0225_0.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2019-08-28 13:37:21'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0225_0.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2019-08-28 13:43:08'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0225_0.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/ljon/jorj/ell3.html',
+				'tnk1/tora/dvrim/dm-24-21.html',
+				NULL,
+				'2019-08-28 15:38:02',
+				'2019-08-28 15:38:02',
+				'דיני עוללות בכרם',
+				'אראל',
+				''
+			)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2019-08-28 15:52:10'
+		WHERE ktovt_bn='tnk1/messages/prqim_t09a18_5.html'
+		;
+
