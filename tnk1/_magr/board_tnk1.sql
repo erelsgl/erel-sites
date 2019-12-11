be8f25dd37f876851323781e622d1780e4dc8e6e
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-12-11 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -32698,3 +32698,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0223_0.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1170.html',
+				NULL,
+				'2019-12-11 06:32:48',
+				'2019-12-11 06:32:48',
+				'פרשת וישלח- מאין שאין לסמוך על הנס?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-12-11 06:32:48'
+			WHERE ktovt_bn='tnk1/forums.html';
+
