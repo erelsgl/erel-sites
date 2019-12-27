5185ee55211867b83ff7d8a2ebe61a0ef61d479a
Erel Segal-Halevi Home <erelsgl@gmail.com>
2019-12-27 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -33557,3 +33557,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t26a3_1.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1175.html',
+				NULL,
+				'2019-12-27 07:18:15',
+				'2019-12-27 07:18:15',
+				' יוסף זוכה לתהילה/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2019-12-27 07:18:15'
+			WHERE ktovt_bn='tnk1/forums.html';
+
