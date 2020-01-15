0e5f24992303a25f5ce557224e7f80090b413a42
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-01-15 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -34048,3 +34048,19 @@ SET character_set_connection=hebrew;
 			)
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/new.html',
+				'tnk1/messages/lxswt_byhwh_mbfwx_bndybym.pdf',
+				NULL,
+				'2020-01-14 19:03:13',
+				'2020-01-14 19:03:13',
+				'טוב לחסות ביהוה מבטוח בנדיבים',
+				'רמי ניר',
+				''
+			)
+		;
+
