85de17a64d7c262ac98a950722aabea6bd170384
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-01-18 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -34663,3 +34663,26 @@ UPDATE board_tnk1
 			SET sdr_bn=60
 			WHERE ktovt_bn='tnk1/messages/forums_1180.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1181.html',
+				NULL,
+				'2020-01-17 12:56:09',
+				'2020-01-17 12:56:09',
+				'הולדת משה/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-01-17 12:56:09'
+			WHERE ktovt_bn='tnk1/forums.html';
+
