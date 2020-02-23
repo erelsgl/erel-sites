5d8598a97f979b8f9845f0ec8f55a21a38cd6e57
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-02-23 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -35565,3 +35565,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2020-02-19 06:43:08'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1191.html',
+				NULL,
+				'2020-02-23 07:48:13',
+				'2020-02-23 07:48:13',
+				' עבד עברייוצא לחופשי/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-02-23 07:48:13'
+			WHERE ktovt_bn='tnk1/forums.html';
+
