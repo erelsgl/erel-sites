8ec4a5ddb0e968d894abbce7b9a415bb1f7e2839
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-03-07 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -35997,3 +35997,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2020-03-05 06:33:44'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1195.html',
+				NULL,
+				'2020-03-06 08:43:05',
+				'2020-03-06 08:43:05',
+				'תרומת שמן הזית/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-03-06 08:43:05'
+			WHERE ktovt_bn='tnk1/forums.html';
+
