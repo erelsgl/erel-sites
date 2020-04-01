d06df9360cdbd2ce51914b332f261d88e0aa4002
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-04-01 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -36435,3 +36435,88 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2020-03-29 19:09:26'
 			WHERE ktovt_bn='tnk1/sofrim/index.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/index.html',
+				'tnk1/messages/punishment.pdf',
+				NULL,
+				'2020-03-31 21:02:37',
+				'2020-03-31 21:02:37',
+				'Punishment',
+				'אבנר רמו',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-03-31 21:02:37'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/index.html',
+				'tnk1/messages/geographical_directions.pdf',
+				NULL,
+				'2020-04-01 02:01:52',
+				'2020-04-01 02:01:52',
+				'Geographical directions',
+				'אבנר רמו',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-04-01 02:01:52'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/msr/knesset/index.html',
+				'tnk1/msr/knesset/justice.html',
+				NULL,
+				'2020-04-01 04:26:28',
+				'2020-04-01 04:26:28',
+				'על סדר היום - רדיפת צדק וירושת הארץ',
+				'אראל',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1202.html',
+				NULL,
+				'2020-04-01 06:52:53',
+				'2020-04-01 06:52:53',
+				'פרשת צו- הבאת מנחה של הכהן הגדול- מהי מסמלת?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-04-01 06:52:53'
+			WHERE ktovt_bn='tnk1/forums.html';
+
