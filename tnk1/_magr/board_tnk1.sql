14a10a86e31f301b9cd2b866d99a342a198ed22b
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-04-25 08:00:01 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -38836,3 +38836,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t26a4_2.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/index.html',
+				'tnk1/messages/david_s_crown.pdf',
+				NULL,
+				'2020-04-24 20:28:51',
+				'2020-04-24 20:28:51',
+				'David\'s crown',
+				'абрш шое',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-04-24 20:28:51'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
