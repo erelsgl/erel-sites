059753dc3338fcd4f3906297dea3a1b5c009a174
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-05-18 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -39210,3 +39210,19 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2020-05-13 14:55:59'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/new.html',
+				'tnk1/messages/ajr_mel_lrqye.pdf',
+				NULL,
+				'2020-05-17 14:44:52',
+				'2020-05-17 14:44:52',
+				'המים אשר מעל לרקיע',
+				'רמי ניר',
+				''
+			)
+		;
+
