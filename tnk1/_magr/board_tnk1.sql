3d86f06c1e579af38c4e22e7620cbb24aadb3ccc
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-05-28 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -39478,3 +39478,26 @@ SET character_set_connection=hebrew;
 			)
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1216.html',
+				NULL,
+				'2020-05-27 09:59:35',
+				'2020-05-27 09:59:35',
+				' חג השבועות- ייחודו ובמה קשור לפרשת נשא?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-05-27 09:59:35'
+			WHERE ktovt_bn='tnk1/forums.html';
+
