fab7fd637b1cae685ef304725c09908f52a9f6ab
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-06-22 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -39967,3 +39967,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0103_0.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/index.html',
+				'tnk1/messages/the_diviner_of_en_dor.pdf',
+				NULL,
+				'2020-06-22 00:00:17',
+				'2020-06-22 00:00:17',
+				'The Diviner of En-dor',
+				'абрш шое',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-06-22 00:00:17'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
