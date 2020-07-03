aa78b050fff0b94e28b3637c2d7ae23d4e60e1fc
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-07-03 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -40219,3 +40219,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/forums_1165.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1227.html',
+				NULL,
+				'2020-07-03 05:17:32',
+				'2020-07-03 05:17:32',
+				'בלעם ובלק/ שיר  מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-07-03 05:17:32'
+			WHERE ktovt_bn='tnk1/forums.html';
+
