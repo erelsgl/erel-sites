9b654979f24a23c443d49b3ea188167296620701
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-08-08 08:00:02 +0000

    daily backup

--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -5492,3 +5492,11 @@ SET character_set_connection=utf8;
 			WHERE parent='tnk1/messages/prqim_t1506_1.html'
 			;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+			UPDATE tguvot_data
+			SET count=count-1, updated_at='2020-08-08 06:45:58'
+			WHERE parent='tnk1/messages/prqim_t1030_2.html'
+			;
+
