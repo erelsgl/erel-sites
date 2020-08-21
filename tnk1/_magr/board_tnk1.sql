22800d44af4f9ed570bb9ad9a735f876631755c4
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-08-21 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -43436,3 +43436,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2020-08-19 21:06:00'
 			WHERE ktovt_bn='tnk1/sofrim/index.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1242.html',
+				NULL,
+				'2020-08-20 21:26:00',
+				'2020-08-20 21:26:00',
+				'להיות תמים עם אלוקים/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-08-20 21:26:00'
+			WHERE ktovt_bn='tnk1/forums.html';
+
