94be8e2a2c1533ca708cf8308de3efdc669fe0f6
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-09-03 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -43791,3 +43791,34 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t1117_1.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2020-09-02 10:44:12'
+		WHERE ktovt_bn='tnk1/ktuv/mjly/mj-31-29.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1245.html',
+				NULL,
+				'2020-09-02 12:09:57',
+				'2020-09-02 12:09:57',
+				'פרשת כי תבוא- ברוך אתה בבואך- אימתי?',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-09-02 12:09:57'
+			WHERE ktovt_bn='tnk1/forums.html';
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -1675,3 +1675,18 @@ SET character_set_connection=utf8;
 			WHERE parent='tnk1/messages/prqim_t1117_1.html'
 			;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/ktuv/mjly/mj-31-29.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2020-09-02 10:44:12'
+		WHERE parent='tnk1/ktuv/mjly/mj-31-29.html'
+		;
+
