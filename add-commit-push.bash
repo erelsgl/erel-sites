327215411f8b9117a36a9c2968d136dfa2719a78
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-09-04 08:01:41 +0300

    Update link to lulu

--- add-commit-push.bash
+++ add-commit-push.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+read COMMIT_MESSAGE
 echo ==== erel-sites ====
 date
 echo -- add --
 /usr/bin/git add -A
 echo -- commit --
-/usr/bin/git commit -m "$1"
+/usr/bin/git commit -m "$COMMIT_MESSAGE"
 echo -- pull --
 /usr/bin/git pull
 echo -- push --
