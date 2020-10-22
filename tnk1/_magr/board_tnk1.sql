66666fac8844937acd9b578f9d01c7a9fa20770c
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-10-22 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -48533,3 +48533,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2020-10-21 05:57:20'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1260.html',
+				NULL,
+				'2020-10-21 15:52:33',
+				'2020-10-21 15:52:33',
+				'נח והבטחת ה\'/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-10-21 15:52:33'
+			WHERE ktovt_bn='tnk1/forums.html';
+
