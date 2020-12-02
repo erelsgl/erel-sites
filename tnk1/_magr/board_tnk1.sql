9c94ff2194d2a8f0541f4d67783079a8207bfb93
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-12-02 08:00:01 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -49413,3 +49413,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/ktuv/mjly/mj-31-29.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1271.html',
+				NULL,
+				'2020-12-02 06:21:33',
+				'2020-12-02 06:21:33',
+				'פרשת וישלח- מדוע חשש יעקב מפני פגישתו עם עשיו?/ אהובה קליין',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-12-02 06:21:33'
+			WHERE ktovt_bn='tnk1/forums.html';
+
