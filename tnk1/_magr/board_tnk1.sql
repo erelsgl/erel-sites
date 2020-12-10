a4d659e7d7c807a84b7d777fd2d72f375c8cb04c
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-12-10 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -49682,3 +49682,34 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t2824_1.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2020-12-09 18:32:49'
+		WHERE ktovt_bn='tnk1/messages/ljon_jorj_fwb_0.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1273.html',
+				NULL,
+				'2020-12-10 06:41:44',
+				'2020-12-10 06:41:44',
+				'פרשת וישב- יוסף מושלך לבור- האחים סועדים- הכיצד?/ אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2020-12-10 06:41:44'
+			WHERE ktovt_bn='tnk1/forums.html';
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -1164,3 +1164,18 @@ SET character_set_connection=utf8;
 			WHERE parent='tnk1/messages/prqim_t2824_1.html'
 			;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/ljon_jorj_fwb_0.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2020-12-09 18:32:49'
+		WHERE parent='tnk1/messages/ljon_jorj_fwb_0.html'
+		;
+
