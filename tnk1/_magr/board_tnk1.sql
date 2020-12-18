2cfeb6839d6886e6c78bdc847666a58227cecea0
Erel Segal-Halevi Home <erelsgl@gmail.com>
2020-12-18 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -50817,3 +50817,19 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t08b23_1.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2020-12-18 04:18:27'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0107_2.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2020-12-18 04:41:04'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0617_0.html'
+		;
+
