93ebab8f460ea12ae484c3d1d1db25ade97d35d7
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-01-09 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -52206,3 +52206,27 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/tora_jmot_byom_o_blyla_1.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2021-01-08 12:10:18'
+		WHERE ktovt_bn='tnk1/messages/prqim_t2805_1.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/new.html',
+				'tnk1/messages/mjwjh_xswm_xwtk_megl_xwsm.pdf',
+				NULL,
+				'2021-01-08 14:49:13',
+				'2021-01-08 14:49:13',
+				'כאשר משושה חסום חותך מעגל חוסם',
+				'רמי ניר',
+				''
+			)
+		;
+
