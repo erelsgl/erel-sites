3b2d007bac1c234c63795f659213b9889b44b374
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-01-18 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -52637,3 +52637,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/mlbim_wy_wy-14-14-mlbim_0.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1284.html',
+				NULL,
+				'2021-01-17 20:12:02',
+				'2021-01-17 20:12:02',
+				' מכת הצפרדעים/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-01-17 20:12:02'
+			WHERE ktovt_bn='tnk1/forums.html';
+
