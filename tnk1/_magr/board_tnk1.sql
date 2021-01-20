1a303036a77f01c06f03352094a3129672c80f98
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-01-20 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -52740,3 +52740,34 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t1302_0.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1285.html',
+				NULL,
+				'2021-01-20 06:37:11',
+				'2021-01-20 06:37:11',
+				' פרשת בא-אכילת מצה- בחג הפסח, לשם מה?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-01-20 06:37:11'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2021-01-20 07:56:28'
+		WHERE ktovt_bn='tnk1/kma/hvdlim1/romx_wxnit.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -5101,3 +5101,18 @@ SET character_set_connection=utf8;
 			WHERE parent='tnk1/messages/prqim_t1302_0.html'
 			;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/kma/hvdlim1/romx_wxnit.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2021-01-20 07:56:28'
+		WHERE parent='tnk1/kma/hvdlim1/romx_wxnit.html'
+		;
+
