b208dfeed7f22fd886ec573bd2ba65316f687ba5
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-02-11 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -54308,3 +54308,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t1404_0.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1291.html',
+				NULL,
+				'2021-02-10 17:04:42',
+				'2021-02-10 17:04:42',
+				'פרשת  משפטים-מהו המתכון- למניעת מגפות וחולי?/ אהובה קליין',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-02-10 17:04:42'
+			WHERE ktovt_bn='tnk1/forums.html';
+
