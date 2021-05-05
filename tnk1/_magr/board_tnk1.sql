9694c83f822a3a13241f6225e5db9609e888018b
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-05-05 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -57271,3 +57271,11 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2021-05-03 16:52:15'
 			WHERE ktovt_bn='tnk1/sofrim/index.html';
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2021-05-05 04:53:05'
+		WHERE ktovt_bn='tnk1/messages/prqim_t2655_5.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -672,3 +672,18 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/messages/prqim_t2602_1.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t2655_5.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2021-05-05 04:53:05'
+		WHERE parent='tnk1/messages/prqim_t2655_5.html'
+		;
+
