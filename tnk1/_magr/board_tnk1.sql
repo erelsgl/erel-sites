0c659e7c5241e50c9cb80326368493b0ea3395f6
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-06-17 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -59353,3 +59353,27 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t1502_2.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/new.html',
+				'tnk1/messages/hmegl_ajr_yswpr_ladwny_ldwr.pdf',
+				NULL,
+				'2021-06-16 16:40:16',
+				'2021-06-16 16:40:16',
+				'סיפור המעגל אשר יסופר לאדוני לדור',
+				'רמי ניר',
+				''
+			)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2021-06-17 04:20:05'
+		WHERE ktovt_bn='tnk1/messages/prqim_t08b23_1.html'
+		;
+
