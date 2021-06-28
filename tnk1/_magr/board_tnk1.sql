c0cdd4487a1ee0f0f609bd30a72371e94c0c80eb
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-06-28 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -59689,3 +59689,34 @@ SET character_set_client=utf8;
 SET character_set_connection=utf8;
 DELETE FROM `board_tnk1` WHERE `ktovt_av` = 'tnk1/sofrim/shabot/index.html' AND `ktovt_bn` = 'tnk1/messages/aaa.png' AND 1 LIMIT 1;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1334.html',
+				NULL,
+				'2021-06-27 14:46:15',
+				'2021-06-27 14:46:15',
+				'מחורבן לגאולה/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-06-27 14:46:15'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2021-06-28 04:59:19'
+		WHERE ktovt_bn='tnk1/klli/mdrjim/mcwot.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -3893,3 +3893,18 @@ SET character_set_connection=utf8;
 			WHERE parent='tnk1/messages/sofrim_shabot_index_132.html'
 			;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/klli/mdrjim/mcwot.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2021-06-28 04:59:19'
+		WHERE parent='tnk1/klli/mdrjim/mcwot.html'
+		;
+
