e66e7f002ee531ca988049decb57806ec39ff057
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-07-07 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -60029,3 +60029,11 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/ljon/dq/poal_yxid_ecm_rbim.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2021-07-06 10:37:01'
+		WHERE ktovt_bn='tnk1/tora/brejit/tarikim25.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -4400,3 +4400,18 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/ljon/dq/poal_yxid_ecm_rbim.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/tora/brejit/tarikim25.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2021-07-06 10:37:01'
+		WHERE parent='tnk1/tora/brejit/tarikim25.html'
+		;
+
