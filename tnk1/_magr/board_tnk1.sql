fd9b61734abb6f29268a56a4f7da55a098f7d088
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-08-31 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -64184,3 +64184,19 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0101_23.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/new.html',
+				'tnk1/messages/knp_al_knp.pdf',
+				NULL,
+				'2021-08-30 13:04:42',
+				'2021-08-30 13:04:42',
+				'נוגעות כנף אל כנף',
+				'רמי ניר',
+				''
+			)
+		;
+
