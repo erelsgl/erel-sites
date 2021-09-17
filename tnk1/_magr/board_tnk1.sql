3c11a342f6be73a16f55b810dde8b6e233cb33c4
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-09-17 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -64532,3 +64532,49 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/tora/dvrim/geula_tokatly.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1357.html',
+				NULL,
+				'2021-09-16 20:44:07',
+				'2021-09-16 20:44:07',
+				'פרשת האזינו-מסרים ונבואות  לישראל בשירת האזינו/ אהובה קליין(c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-09-16 20:44:07'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1358.html',
+				NULL,
+				'2021-09-16 20:47:48',
+				'2021-09-16 20:47:48',
+				'שירה לעם הנבחר/ אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-09-16 20:47:48'
+			WHERE ktovt_bn='tnk1/forums.html';
+
