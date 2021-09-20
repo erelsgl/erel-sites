ed90f253d8e02975e1117b262d04bffb42d20ccb
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-09-20 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -64578,3 +64578,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2021-09-16 20:47:48'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1359.html',
+				NULL,
+				'2021-09-20 07:57:37',
+				'2021-09-20 07:57:37',
+				'שלמה המלך והים/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-09-20 07:57:37'
+			WHERE ktovt_bn='tnk1/forums.html';
+
