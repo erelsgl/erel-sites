85d4f8b6596b8684501179dcc22dbe0e1d7db541
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-09-21 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -64601,3 +64601,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2021-09-20 07:57:37'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1360.html',
+				NULL,
+				'2021-09-20 08:00:37',
+				'2021-09-20 08:00:37',
+				'חג הסוכות ומגילת קהלת- מה הקשר?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-09-20 08:00:37'
+			WHERE ktovt_bn='tnk1/forums.html';
+
