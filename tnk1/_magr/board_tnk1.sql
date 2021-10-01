20007b5ff4c47df51695236d02a1f510a7c5394b
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-10-01 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -65582,3 +65582,49 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/tora/dvrim/dm-04-20.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1362.html',
+				NULL,
+				'2021-09-30 11:59:24',
+				'2021-09-30 11:59:24',
+				'פרשת בראשית- מה הם האוצרות הגנוזים בתורה הקדושה?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-09-30 11:59:24'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1363.html',
+				NULL,
+				'2021-09-30 12:04:31',
+				'2021-09-30 12:04:31',
+				'הבריאה ביום החמישי/ שיר מאת אהובה קליין: ',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-09-30 12:04:31'
+			WHERE ktovt_bn='tnk1/forums.html';
+
