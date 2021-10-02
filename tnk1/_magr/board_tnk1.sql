835bb30a50a26664543391bb6d101869e453b357
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-10-02 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -65628,3 +65628,34 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2021-09-30 12:04:31'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2021-10-01 13:14:22'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0231_0.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/index.html',
+				'tnk1/messages/the_passover_s_sacrifice_to_yhwh_and_the_celebration_of_the_unleavened_bread.pdf',
+				NULL,
+				'2021-10-01 18:00:10',
+				'2021-10-01 18:00:10',
+				'The Passover\'s sacrifice to YHWH and the celebration of the unleavened bread',
+				'абрш шое',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-10-01 18:00:10'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -3286,3 +3286,26 @@ SET character_set_connection=utf8;
 		WHERE parent='tnk1/tora/dvrim/dm-04-20.html'
 		;
 
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t0231_0.html',0)
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2021-10-01 13:14:22'
+		WHERE parent='tnk1/messages/prqim_t0231_0.html'
+		;
+
+SET character_set_client=utf8;
+SET character_set_connection=utf8;
+
+			UPDATE tguvot_data
+			SET count=count-1, updated_at='2021-10-01 13:14:23'
+			WHERE parent='tnk1/messages/prqim_t0231_0.html'
+			;
+
