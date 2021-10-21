9db43f75ab5e5b9b0296df67783b6096966b2678
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-10-21 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -65919,3 +65919,26 @@ SET character_set_connection=utf8;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0223_0.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1368.html',
+				NULL,
+				'2021-10-20 16:19:16',
+				'2021-10-20 16:19:16',
+				' פרשת וירא- אברהם מבצע את רצון שרה-כיצד?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-10-20 16:19:16'
+			WHERE ktovt_bn='tnk1/forums.html';
+
