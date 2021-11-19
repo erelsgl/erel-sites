3569ec9041858cb98fe28c6dbb1c383c9a6f243d
Erel Segal-Halevi Home <erelsgl@gmail.com>
2021-11-19 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -66956,3 +66956,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2021-11-18 07:23:41'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1377.html',
+				NULL,
+				'2021-11-18 21:24:59',
+				'2021-11-18 21:24:59',
+				'יעקב נפרד מעשיו/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-11-18 21:24:59'
+			WHERE ktovt_bn='tnk1/forums.html';
+
