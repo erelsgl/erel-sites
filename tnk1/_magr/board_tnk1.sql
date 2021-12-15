f80e0305f267b69fa84994d38d75e59fc3236787
Erel Segal-Halevi Server <erelsgl@gmail.com>
2021-12-15 08:00:08 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -763,3 +763,26 @@ SET character_set_connection=hebrew;
 			)
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1386.html',
+				NULL,
+				'2021-12-14 17:12:31',
+				'2021-12-14 17:12:31',
+				' פרשת ויחי- מהו סוד הגאולה?/ מאת: אהובה קליין ',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-12-14 17:12:31'
+			WHERE ktovt_bn='tnk1/forums.html';
+
