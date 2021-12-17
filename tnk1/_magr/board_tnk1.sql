73947f6fa6f14e80083e9600ac969422470886e0
Erel Segal-Halevi Server <erelsgl@gmail.com>
2021-12-17 08:00:08 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -786,3 +786,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2021-12-14 17:12:31'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1387.html',
+				NULL,
+				'2021-12-17 04:56:16',
+				'2021-12-17 04:56:16',
+				'שבט דן וברכתו/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2021-12-17 04:56:16'
+			WHERE ktovt_bn='tnk1/forums.html';
+
