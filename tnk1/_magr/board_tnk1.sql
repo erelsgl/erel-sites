352ecf5a67386c473557b7eb39c3bcf24dcf068a
Erel Segal-Halevi Server <erelsgl@gmail.com>
2022-01-21 08:00:05 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -1652,3 +1652,26 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/nvia/yjayhu/yj-06-0910.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1397.html',
+				NULL,
+				'2022-01-21 06:21:51',
+				'2022-01-21 06:21:51',
+				'יתרו והשמועה/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2022-01-21 06:21:51'
+			WHERE ktovt_bn='tnk1/forums.html';
+
