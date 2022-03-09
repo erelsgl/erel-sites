f19ae0ba2d300e6d0aa106a1c3e552c8a1d981c0
Erel Segal-Halevi Server <erelsgl@gmail.com>
2022-03-09 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -3866,3 +3866,11 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/ktuv/mgilot/tklt_avrhm_mskt.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2022-03-09 06:57:45'
+		WHERE ktovt_bn='tnk1/nvia/yjayhu/yj-02-04.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -2811,3 +2811,26 @@ SET character_set_connection=utf8mb3;
 		WHERE parent='tnk1/ktuv/mgilot/tklt_avrhm_mskt.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/nvia/yjayhu/yj-02-04.html',0)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2022-03-09 06:57:45'
+		WHERE parent='tnk1/nvia/yjayhu/yj-02-04.html'
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+			UPDATE tguvot_data
+			SET count=count-1, updated_at='2022-03-09 06:57:51'
+			WHERE parent='tnk1/nvia/yjayhu/yj-02-04.html'
+			;
+
