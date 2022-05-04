cc755d791b6c9a1fa612783776a6d872f5d701f4
Erel Segal-Halevi Server <erelsgl@gmail.com>
2022-05-04 08:00:06 +0000

    daily backup

--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -4411,3 +4411,18 @@ SET character_set_connection=utf8mb3;
 		WHERE parent='tryg/ribit0/gmx_hsbr.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tryg/ribit0/get.html',0)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2022-05-03 11:04:38'
+		WHERE parent='tryg/ribit0/get.html'
+		;
+
