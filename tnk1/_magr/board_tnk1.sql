b141a68aa0ef9442815e50ef548cd53f3fbc2d3c
Erel Segal-Halevi Server <erelsgl@gmail.com>
2022-05-09 08:00:05 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -7175,3 +7175,19 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/nvia/yrmyhu/yr-12-0103.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/dmut/dmut/32.html',
+				'tnk1/messages/dmut_dmut_32_0.html',
+				NULL,
+				'2022-05-08 15:51:52',
+				'2022-05-08 15:51:52',
+				'נא אמון - תבאי ',
+				'אביתר כהן',
+				''
+			)
+		;
+
