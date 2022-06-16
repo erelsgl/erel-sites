dd714e7088e010384b7ad36a70d53e6aca3eea7f
Erel Segal-Halevi Server <erelsgl@gmail.com>
2022-06-16 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -7507,3 +7507,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2022-06-14 19:12:38'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1449.html',
+				NULL,
+				'2022-06-15 14:28:32',
+				'2022-06-15 14:28:32',
+				'פרשת שלח-המרגלים- והקשר להפטרה?/ אהובה קליין',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2022-06-15 14:28:32'
+			WHERE ktovt_bn='tnk1/forums.html';
+
