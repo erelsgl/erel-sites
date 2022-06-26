f30abbc1feefd34c9e2bf5d1c08f235dc0d73f06
Erel Segal-Halevi Server <erelsgl@gmail.com>
2022-06-26 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -7592,3 +7592,26 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/tora/bmdbr/prjt_qorx.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/sofrim/index.html',
+				'tnk1/messages/the_roots_drop_off__leave__and_uproot.pdf',
+				NULL,
+				'2022-06-26 00:48:37',
+				'2022-06-26 00:48:37',
+				'The roots drop off; leave, and uproot',
+				'абрш шое',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2022-06-26 00:48:37'
+			WHERE ktovt_bn='tnk1/sofrim/index.html';
+
