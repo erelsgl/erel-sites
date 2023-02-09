e340c3030a7ea98aeb8298da481e95c05b710e9b
Erel Segal-Halevi Server <erelsgl@gmail.com>
2023-02-09 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -14221,3 +14221,49 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0234_1.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1519.html',
+				NULL,
+				'2023-02-08 22:57:18',
+				'2023-02-08 22:57:18',
+				'פרשת יתרו- מה עורר את יתרו לבוא אל משה?/ אהובה קליין',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2023-02-08 22:57:18'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1520.html',
+				NULL,
+				'2023-02-08 22:59:43',
+				'2023-02-08 22:59:43',
+				'יתרו ומשה/ שיר מאת: אהובה קליין (c)',
+				' אהובה קליין.',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2023-02-08 22:59:43'
+			WHERE ktovt_bn='tnk1/forums.html';
+
