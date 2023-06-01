9c7febc360da77b68daa5d05546b0767e3dc3776
Erel Segal-Halevi Server <erelsgl@gmail.com>
2023-06-01 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -48,3 +48,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2023-05-30 20:23:47'
 			WHERE ktovt_bn='tnk1/sofrim/index.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1551.html',
+				NULL,
+				'2023-05-31 21:31:37',
+				'2023-05-31 21:31:37',
+				'פרשת בהעלותך-גדולת משה ותכלית החצוצרות/ אהובה קלייו',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2023-05-31 21:31:37'
+			WHERE ktovt_bn='tnk1/forums.html';
+
