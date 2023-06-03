da22eb0b679e0673eb23e2a5c278ece5756b1662
Erel Segal-Halevi Server <erelsgl@gmail.com>
2023-06-03 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -79,3 +79,34 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/forums_1551.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2023-06-02 12:51:50'
+		WHERE ktovt_bn='tnk1/nvir/joftim/loxmim.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1552.html',
+				NULL,
+				'2023-06-02 14:28:04',
+				'2023-06-02 14:28:04',
+				'ציווי החצוצרות למשה/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2023-06-02 14:28:04'
+			WHERE ktovt_bn='tnk1/forums.html';
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -93,3 +93,18 @@ SET character_set_connection=utf8mb3;
 			WHERE parent='tnk1/messages/forums_1551.html'
 			;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/nvir/joftim/loxmim.html',0)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2023-06-02 12:51:50'
+		WHERE parent='tnk1/nvir/joftim/loxmim.html'
+		;
+
