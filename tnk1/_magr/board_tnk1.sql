891ed300643e9aa2b01d81eb135b4f8150588471
Erel Segal-Halevi Server <erelsgl@gmail.com>
2023-08-03 08:00:05 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -940,3 +940,26 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0101_23.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1570.html',
+				NULL,
+				'2023-08-03 05:40:00',
+				'2023-08-03 05:40:00',
+				'פרשת עקב-כיצד להתמודד עם מלחמה רב מערכתית?/ אהובה קליין?',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2023-08-03 05:40:00'
+			WHERE ktovt_bn='tnk1/forums.html';
+
