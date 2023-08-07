11dface885eb8fa5424c296ece93c9babd8de396
Erel Segal-Halevi Server <erelsgl@gmail.com>
2023-08-07 08:00:07 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -986,3 +986,11 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2023-08-04 09:10:37'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2023-08-06 16:54:33'
+		WHERE ktovt_bn='tnk1/messages/daian2_0.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -1002,3 +1002,18 @@ SET character_set_connection=utf8mb3;
 			WHERE parent='tnk1/messages/prqim_t1402_2.html'
 			;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/daian2_0.html',0)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2023-08-06 16:54:33'
+		WHERE parent='tnk1/messages/daian2_0.html'
+		;
+
