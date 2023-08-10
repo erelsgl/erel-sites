36bee40fb39f669550a98f006c94cc16480de320
Erel Segal-Halevi Server <erelsgl@gmail.com>
2023-08-10 08:00:05 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -994,3 +994,26 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/daian2_0.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1573.html',
+				NULL,
+				'2023-08-09 20:48:03',
+				'2023-08-09 20:48:03',
+				'פרשת ראה, עם ישראל- בנים לה\' ועם סגולה ,כיצד?/ אהובה קליין',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2023-08-09 20:48:03'
+			WHERE ktovt_bn='tnk1/forums.html';
+
