d9d3c7c6ae20b21f61bf392bc48d259c79f6ef7a
Erel Segal-Halevi Server <erelsgl@gmail.com>
2023-08-21 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -1190,3 +1190,26 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/kma/qjrim1/byt.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/msr/1israel.html',
+				'tnk1/tora/jmot/jm-19-06.html',
+				NULL,
+				'2023-08-21 02:07:30',
+				'2023-08-21 02:07:30',
+				'ממלכת כהנים וגוי קדוש',
+				'אראל',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2023-08-21 02:07:30'
+			WHERE ktovt_bn='tnk1/msr/1israel.html';
+
