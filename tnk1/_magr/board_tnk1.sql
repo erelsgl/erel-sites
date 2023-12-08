925f533f4152e5d9cad87c8120888d69d7fee445
Erel Segal-Halevi Server <erelsgl@gmail.com>
2023-12-08 08:00:06 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -6439,3 +6439,49 @@ SET character_set_client=utf8mb3;
 SET character_set_connection=utf8mb3;
 UPDATE `board_tnk1` SET `sdr_bn` = NULL WHERE `ktovt_av` = 'tryg/sug/cycyt.html' AND `ktovt_bn` = 'axrimpl/hydepark/2229966_7.html' AND 1 LIMIT 1;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1605.html',
+				NULL,
+				'2023-12-07 11:37:37',
+				'2023-12-07 11:37:37',
+				'פרשת  וישב- הסוד להצלחה בניסיונות החיים/ אהובה קליין',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2023-12-07 11:37:37'
+			WHERE ktovt_bn='tnk1/forums.html';
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1606.html',
+				NULL,
+				'2023-12-07 11:41:58',
+				'2023-12-07 11:41:58',
+				'יעקב והמנחמים/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2023-12-07 11:41:58'
+			WHERE ktovt_bn='tnk1/forums.html';
+
