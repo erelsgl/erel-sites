c4bc033ee7598423744d7d913aacf4eb27403873
Erel Segal-Halevi Server <erelsgl@gmail.com>
2024-01-26 08:00:12 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -8101,3 +8101,11 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2024-01-24 21:13:19'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2024-01-25 13:34:38'
+		WHERE ktovt_bn='tnk1/messages/forums_1620.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -4129,3 +4129,18 @@ SET character_set_connection=utf8mb3;
 		WHERE parent='tnk1/kma/qjrim2/mje_wqyn.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/forums_1620.html',0)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2024-01-25 13:34:38'
+		WHERE parent='tnk1/messages/forums_1620.html'
+		;
+
