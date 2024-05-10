7afa7c8fdc8db0ba689589e9ab3dfe26af382292
Erel Segal-Halevi Server <erelsgl@gmail.com>
2024-05-10 08:00:07 +0000

    daily backup

--- tnk1/_magr/tguvot.sql
+++ tnk1/_magr/tguvot.sql
@@ -6085,3 +6085,22 @@ SET character_set_connection=utf8mb3;
 			SET deleted_at='2024-05-09 04:11:47' 
 			WHERE messageid=12743;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT INTO tguvot (parent,body,userid,username,created_at) VALUES (
+		'tnk1/messages/prqim_t0108_1.html',
+		'<p>.</p>',
+		'115934331744503251746',
+		'DAIAN SHEM',
+		'2024-05-10 07:16:32'
+		)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+			UPDATE tguvot 
+			SET deleted_at='2024-05-10 07:16:34' 
+			WHERE messageid=12744;
+
