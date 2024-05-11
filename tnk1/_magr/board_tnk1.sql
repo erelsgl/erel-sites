a5a85a69a580dbae0add7f3596c93e5338203aa1
Erel Segal-Halevi Server <erelsgl@gmail.com>
2024-05-11 08:00:06 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -11700,3 +11700,26 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0108_1.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1654.html',
+				NULL,
+				'2024-05-10 12:15:00',
+				'2024-05-10 12:15:00',
+				'הנביא עמוס ובשורתו/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2024-05-10 12:15:00'
+			WHERE ktovt_bn='tnk1/forums.html';
+
