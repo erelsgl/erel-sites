32565e53e599b0c38b3595715a2ed9808f50ff92
Erel Segal-Halevi Server <erelsgl@gmail.com>
2024-06-22 08:00:08 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -13190,3 +13190,26 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/tora/wyqra/wy-26-23.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1666.html',
+				NULL,
+				'2024-06-21 13:24:46',
+				'2024-06-21 13:24:46',
+				'האספסוף בפני משה/ שיר מאת: אהובה קליין(c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2024-06-21 13:24:46'
+			WHERE ktovt_bn='tnk1/forums.html';
+
