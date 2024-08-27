9614e1ffaab3bc1a76f460bdcf417c8a53e772d8
Erel Segal-Halevi Server <erelsgl@gmail.com>
2024-08-27 08:00:08 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -16033,3 +16033,19 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/prqim_t1201_1.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2024-08-26 18:50:52'
+		WHERE ktovt_bn='tnk1/messages/prqim_t2824_1.html'
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2024-08-26 18:56:15'
+		WHERE ktovt_bn='tnk1/messages/prqim_t2824_1.html'
+		;
+
