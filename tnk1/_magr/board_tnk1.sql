62ead30b52d3c4eb1b021d75a4168eec9d5f7f67
Erel Segal-Halevi Server <erelsgl@gmail.com>
2024-11-02 08:00:03 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -18028,3 +18028,11 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2024-10-31 21:07:34'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2024-11-01 13:24:40'
+		WHERE ktovt_bn='tnk1/messages/daian2_0.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -1971,3 +1971,26 @@ SET character_set_connection=utf8mb3;
 			WHERE parent='tnk1/tora/bmdbr/bm-13-33.html'
 			;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/daian2_0.html',0)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2024-11-01 13:24:40'
+		WHERE parent='tnk1/messages/daian2_0.html'
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+			UPDATE tguvot_data
+			SET count=count-1, updated_at='2024-11-01 13:24:47'
+			WHERE parent='tnk1/messages/daian2_0.html'
+			;
+
