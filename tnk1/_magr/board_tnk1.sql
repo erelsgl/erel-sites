3a5f198c7b56f7eecfbeb3a027f467013df561de
Erel Segal-Halevi Server <erelsgl@gmail.com>
2025-01-11 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -20922,3 +20922,11 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2025-01-09 21:49:54'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2025-01-10 13:40:11'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0146_2.html'
+		;
+
--- tnk1/_magr/tguvot.sql
+++ tnk1/_magr/tguvot.sql
@@ -13146,3 +13146,22 @@ SET character_set_connection=utf8mb3;
 		)
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT INTO tguvot (parent,body,userid,username,created_at) VALUES (
+		'tnk1/messages/prqim_t0146_2.html',
+		'<p></p>',
+		'115934331744503251746',
+		'DAIAN SHEM',
+		'2025-01-10 13:40:11'
+		)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+			UPDATE tguvot 
+			SET deleted_at='2025-01-10 13:40:13' 
+			WHERE messageid=13186;
+
