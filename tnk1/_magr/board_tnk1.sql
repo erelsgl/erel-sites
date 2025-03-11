2c92bb9b9c72b919bd9952ad08302fa4ccbbf7f2
Erel Segal-Halevi Server <erelsgl@gmail.com>
2025-03-11 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -21897,3 +21897,11 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/daian2_0.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2025-03-10 08:36:29'
+		WHERE ktovt_bn='tnk1/messages/prqim_t0516_6.html'
+		;
+
