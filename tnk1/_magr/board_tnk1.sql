7b6c18950b2f112981316dab49c4e9c59f54dbdd
Erel Segal-Halevi Server <erelsgl@gmail.com>
2025-05-02 08:00:04 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -22954,3 +22954,34 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/prqim_t1001_4.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2025-05-01 14:20:18'
+		WHERE ktovt_bn='tnk1/messages/prqim_t2619_1.html'
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1755.html',
+				NULL,
+				'2025-05-01 22:20:17',
+				'2025-05-01 22:20:17',
+				'פרשת: תזריע- מצורע-מי היו ארבעת המצורעים?/ מאת: אהובה קליי',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2025-05-01 22:20:17'
+			WHERE ktovt_bn='tnk1/forums.html';
+
