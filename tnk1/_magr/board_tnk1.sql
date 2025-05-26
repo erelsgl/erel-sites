f0fa439b05e1a00ad686c3b37154a94c28360a3a
Erel Segal-Halevi Server <erelsgl@gmail.com>
2025-05-26 08:00:05 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -23649,3 +23649,11 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/dmut/ToknLfiDmutIja.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2025-05-26 06:58:42'
+		WHERE ktovt_bn='tnk1/messages/prqim_t1404_0.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -2098,3 +2098,18 @@ SET character_set_connection=utf8mb3;
 		WHERE parent='tnk1/dmut/ToknLfiDmutIja.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/messages/prqim_t1404_0.html',0)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2025-05-26 06:58:42'
+		WHERE parent='tnk1/messages/prqim_t1404_0.html'
+		;
+
