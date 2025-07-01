1b5511976b054624ef28a33142bb5ec792b851f3
Erel Segal-Halevi <erelsgl@gmail.com>
2025-07-01 07:26:15 +0300

    remove rte, wymeditor, and other old stuff

--- _script/vars0.js
+++ _script/vars0.js
@@ -15,6 +15,3 @@ for (i=0; i<path_from_root_to_document.length; ++i)
 var path_from_document_to_root = '';
 for (i=0; i<relative_depth; ++i)
 	path_from_document_to_root += '../';
-/*
-/_script/combine_js.php?files=_script/jquery-3.7.1.min+_script/jquery.hoverIntent.min+_script/vars0+_script/cookies+_script/rtelang/en+_script/rte+_script/elements+_script/form_validation+_script/fields+_script/arguments+_script/sites+_script/dates/dates+_script/templates+_script/magrim+http://www.google.com/friendconnect/script/friendconnect.js+_script/search+_script/etc+_script/jquery.taconite&out=_script/vars&minify=1
-*/
