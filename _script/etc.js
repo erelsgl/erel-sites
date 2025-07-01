1fc288da101269906f141e5750ad20ebfa70c131
Erel Segal-Halevi Server <erelsgl@gmail.com>
2025-07-01 04:28:00 +0000

    remove google ads

--- _script/etc.js
+++ _script/etc.js
@@ -217,16 +217,18 @@ function hide_old_idfields() {
 // the header of every document
 function kotrt() {
 
+/*
 	if (!is_local) {
 		if (/צחור/.test(theAuthor) || /אביתר/.test(theAuthor) || /הופר/.test(theAuthor))  {
 			google_ad_client = "pub-4131841895603404";
-			/* 728x90, נוצר 11/05/08 */
+			// 728x90, נוצר 11/05/08 
 			google_ad_slot = "5267744417";
 			google_ad_width = 728;
 			google_ad_height = 90;
 			document.write('<script type="text/javascript" src="https://pagead2.googlesyndication.com/pagead/show_ads.js"></script>');
 		} 
 	}
+*/
 
 	var theText="";
 	if (theReceiver=='סגלות משלי') {
