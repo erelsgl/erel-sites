0924abd47e9579a8847900079592de8f873b8548
Erel Segal-Halevi Server <erelsgl@gmail.com>
2025-08-03 08:00:01 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -871,3 +871,27 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/prqim_t0146_2.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/ljon/jorj/prsj.html',
+				'tnk1/ktuv/mgilot/ei-01-17.html',
+				NULL,
+				'2025-08-02 21:06:13',
+				'2025-08-02 21:06:13',
+				'ירושלים פורשת יד לעזרה ואין מנחם',
+				'אראל',
+				''
+			)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2025-08-02 22:55:39'
+		WHERE ktovt_bn='tnk1/tora/brejit/br-16-16.html'
+		;
+
--- tnk1/_magr/tguvot_data.sql
+++ tnk1/_magr/tguvot_data.sql
@@ -1767,3 +1767,18 @@ SET character_set_connection=utf8mb3;
 			WHERE parent='tnk1/messages/prqim_t0146_2.html'
 			;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		INSERT IGNORE INTO tguvot_data(parent,count)
+		VALUES('tnk1/tora/brejit/br-16-16.html',0)
+		;
+
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE tguvot_data
+		SET count=count+1, updated_at='2025-08-02 22:55:39'
+		WHERE parent='tnk1/tora/brejit/br-16-16.html'
+		;
+
