0c2da4449c8ec88a4e19d058121c6840415122d2
Erel Segal-Halevi Server <erelsgl@gmail.com>
2025-10-14 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -2727,3 +2727,26 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/msr/yovl.html'
 		;
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1785.html',
+				NULL,
+				'2025-10-13 14:10:44',
+				'2025-10-13 14:10:44',
+				'פרשת:\"וזאת הברכה\"-שמחת תורה, הגשמת הברכה, כיצד?/ אהובה קליין',
+				'אהובה קליין ',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2025-10-13 14:10:44'
+			WHERE ktovt_bn='tnk1/forums.html';
+
