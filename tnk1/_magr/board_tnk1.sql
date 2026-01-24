ea4be7a99d9b9b3d61d4a0fada46d51bba49d61d
Erel Segal-Halevi Server <erelsgl@gmail.com>
2026-01-24 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -4777,3 +4777,26 @@ SET character_set_connection=hebrew;
 			SET newest_child_created_at='2026-01-22 22:26:54'
 			WHERE ktovt_bn='tnk1/forums.html';
 
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+		INSERT INTO board_tnk1 (ktovt_av, ktovt_bn, sdr_bn, created_at, newest_child_created_at, kotrt, m, l)
+		VALUES (
+				'tnk1/forums.html',
+				'tnk1/messages/forums_1815.html',
+				NULL,
+				'2026-01-23 12:56:55',
+				'2026-01-23 12:56:55',
+				'אלוקים מבטיח ומקיים/ שיר מאת: אהובה קליין (c)',
+				'אהובה קליין',
+				''
+			)
+		;
+
+SET character_set_client=hebrew;
+SET character_set_connection=hebrew;
+
+			UPDATE board_tnk1
+			SET newest_child_created_at='2026-01-23 12:56:55'
+			WHERE ktovt_bn='tnk1/forums.html';
+
