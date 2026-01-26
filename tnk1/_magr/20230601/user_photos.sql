a2d67599014955ade3b6bc7607f054581678f522
Erel Segal-Halevi Server <erelsgl@gmail.com>
2026-01-26 00:57:38 +0000

    remove old 20230601 database

--- tnk1/_magr/20230601/user_photos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `user_photos` (
-  `name` varchar(255) NOT NULL,
-  `photo` text,
-  PRIMARY KEY (`name`)
-) ENGINE=MyISAM CHARACTER SET utf8mb3;
-
-LOAD DATA LOCAL INFILE '$BACKUP_FILEROOT/user_photos.txt'  INTO TABLE user_photos (name,photo);
-
