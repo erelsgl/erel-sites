6a853152c9543ed49629e671e26df1476079fc23
Erel Segal-Halevi Server <erelsgl@gmail.com>
2026-05-26 08:00:02 +0000

    daily backup

--- tnk1/_magr/board_tnk1.sql
+++ tnk1/_magr/board_tnk1.sql
@@ -5066,3 +5066,11 @@ SET character_set_connection=utf8mb3;
 		WHERE ktovt_bn='tnk1/messages/prqim_t26a4_2.html'
 		;
 
+SET character_set_client=utf8mb3;
+SET character_set_connection=utf8mb3;
+
+		UPDATE board_tnk1
+		SET newest_child_created_at='2026-05-25 18:08:02'
+		WHERE ktovt_bn='tnk1/tora/brejit/clm_wdmut.html'
+		;
+
